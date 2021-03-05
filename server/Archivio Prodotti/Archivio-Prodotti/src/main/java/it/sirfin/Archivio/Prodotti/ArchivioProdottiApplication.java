e7adfd4bd925697d6e5c4c01e409d418b7cb3cb6
marcobonaccorso <marco.bonaccorso00@gmail.com>
2021-03-05 17:56:43 +0100

    Creazione scheletro del progetto
    abbiamo creato lo scheletro della parte server e stiamo implementando il component per la parte client

---
+++ server/Archivio Prodotti/Archivio-Prodotti/src/main/java/it/sirfin/Archivio/Prodotti/ArchivioProdottiApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.Archivio.Prodotti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivioProdottiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivioProdottiApplication.class, args);
+	}
+
+}
---
+++ server/Archivio Prodotti/Archivio-Prodotti/src/test/java/it/sirfin/Archivio/Prodotti/ArchivioProdottiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.Archivio.Prodotti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivioProdottiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
